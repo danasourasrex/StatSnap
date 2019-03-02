a1764ff1e14149bcc34370383fb4bac7a95f16b4
michaelwojtyna <michaelwojtyna>
2019-03-02 15:40:22 -0500

    added the ability to upload .db and .png files to see how bad it was to upload files to the webpage.

--- StatSnapFlaskController.py
+++ StatSnapFlaskController.py
@@ -1,9 +1,45 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, flash, render_template,request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+UPLOAD_FOLDER = './db_upload'
+ALLOWED_EXTENSIONS = set(['db', 'png'])
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/image_loaded/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+    msg = 'Please select a valid file.'
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        if 'file' not in request.files:
+            flash(msg)
+            return render_template("index.html")
+        file = request.files['file']
+        if file.filename == '':
+            flash(msg)
+            return render_template("index.html")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(app.config['UPLOAD_FOLDER'])
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+        else:
+            flash(msg)
+
+
+
 
 if __name__ == "__main__":
     app.run()
