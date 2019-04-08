e76d9de5f33ed1e9a14c48d0bbc176afdfd7ebef
michaelwojtyna <michaelwojtyna>
2019-04-08 19:42:53 -0400

    good userDAO

---
+++ database/UserDAO.py
@@ -0,0 +1,27 @@
+import cx_Oracle
+from database.DAO import DAO
+from database.User import User
+
+
+class HandleDAO(DAO):
+
+    def __init__(self):
+        DAO.__init__(self)
+
+    def insert(self, data):
+        User.user_instance_checker(data)
+        command_string = "insert into STAT_SNAP_USER(USER_ID, PASSWORD) values (" \
+                         ":1,:2)"
+        self.cur.execute(command_string, (str(data.get_user_id()),
+                                          str(data.get_password())))
+        self.con.commit()
+
+    def select(self, key):
+        command_string = "select USER_ID, PASSWORD from STAT_SNAP_USER where USER_ID = :1;"
+        self.cur.execute(command_string, (str(key)))
+        self.con.commit()
+
+    def delete(self, key):
+        command_string = "delete from STAT_SNAP_USER where USER_ID = :1;"
+        self.cur.execute(command_string, (str(key)))
+        self.con.commit()
