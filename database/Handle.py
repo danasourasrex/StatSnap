a8d121ebd3cfa39da9df5d92ce5bf0b5c7011361
Danasourasrex <danasourasrex>
2019-04-08 19:03:06 -0400

    handle and stubbed handleDAO

---
+++ database/Handle.py
@@ -0,0 +1,39 @@
+class Handle:
+
+    def __init__(self):
+        self.__handle_id = -1
+        self.__phone_number = ""
+        self.__user_id = -1
+
+    def get_dictionary(self):
+        return {"HANDLE_ID": self.get_handle_id(),
+                "PHONE_NUMBER": self.get_phone_number(),
+                "USER_ID": self.get_user_id()}
+
+    def set_values_from_row(self, row):
+        self.set_handle_id(row[0])
+        self.set_phone_number(row[1])
+        self.set_user_id(row[2])
+
+    def get_handle_id(self):
+        return self.__handle_id
+
+    def get_phone_number(self):
+        return self.__phone_number
+
+    def get_user_id(self):
+        return self.__user_id
+
+    def set_handle_id(self, data):
+        self.__handle_id = data
+
+    def set_phone_number(self, data):
+        self.__phone_number = data
+
+    def set_user_id(self, data):
+        self.__user_id = data
+
+    @staticmethod
+    def handle_instance_checker(item):
+        if not isinstance(item, Handle):
+            raise ValueError('Item is not a Handle')
---
+++ database/HandleDAO.py
@@ -0,0 +1,10 @@
+import cx_Oracle
+from database.DAO import DAO
+
+
+class HandleDAO(DAO):
+
+    def __init__(self):
+        DAO.__init__(self)
+
+
