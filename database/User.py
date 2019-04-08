aa35e6115b0383a7db67936cf942c6c397684be9
michaelwojtyna <michaelwojtyna>
2019-04-08 19:37:47 -0400

    added create_account and login user class and an empty user dao

---
+++ database/User.py
@@ -0,0 +1,30 @@
+class User:
+
+    def __init__(self):
+        self.__user_id = ""
+        self.__password = ""
+
+    def get_dictionary(self):
+        return {"USER_ID": self.get_handle_id(),
+                "PASSWORD": self.get_phone_number()}
+
+    def set_values_from_row(self, row):
+        self.set_user_id(row[0])
+        self.set_password(row[1])
+
+    def get_user_id(self):
+        return self.__user_id
+
+    def get_phone_number(self):
+        return self.__password
+
+    def set_user_id(self, data):
+        self.__user_id = data
+
+    def set_password(self, data):
+        self.__password = data
+
+    @staticmethod
+    def user_instance_checker(item):
+        if not isinstance(item, User):
+            raise ValueError('Item is not a User')
