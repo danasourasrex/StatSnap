f3597c3aab1a4698735257a4dacefb73288a6547
Danasourasrex <danasourasrex>
2019-04-08 19:11:31 -0400

    master dao stuff

--- database/DAO.py
+++ database/DAO.py
@@ -4,7 +4,16 @@ import cx_Oracle
 class DAO:
 
     def __init__(self):
-        pass
+        ip = 'stonehillcsc325.cjjvanphib99.us-west-2.rds.amazonaws.com'
+        port = 1521
+        SID = 'ORCL'
+        dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+        self.con = cx_Oracle.connect('mwojtyna', 'csrocks55', dsn_tns)
+        self.cur = self.con.cursor()
+
+    def __del__(self):
+        self.cur.close()
+        self.con.close()
 
     def select(self, data):
         pass
