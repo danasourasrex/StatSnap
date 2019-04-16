c5554c80516c6cff8e9bff8ea31cbf3f30979ad9
michaelwojtyna <michaelwojtyna>
2019-04-15 23:40:28 -0400

    added delete and select

--- database/ExpandedDataDAO.py
+++ database/ExpandedDataDAO.py
@@ -10,3 +10,16 @@ class ExpandedDataDAO(DAO):
         command_string = "INSERT INTO EXPANDED_DATA (STAT_ID, DATA, OCCURRENCES) VALUES  (:1,:2,:3)"
         self.cur.execute(command_string, (str(item.get_stat_id), str(item.get_data), str(item.get_occurrences())))
         self.con.commit()
+
+    def select(self, key):
+        command_string = "SELECT * FROM EXPANDED_DATA WHERE STAT_ID = " + str(key)
+        self.cur.execute(command_string)
+        row = self.cur.fetchone()
+        expanded_data = ExpandedData()
+        expanded_data.set_values_from_row(row)
+        return expanded_data
+
+    def delete(self, key):
+        command_string = "DELETE FROM EXPANDED_DATA WHERE STAT_ID = " + str(key)
+        self.cur.execute(command_string)
+        self.con.commit()
